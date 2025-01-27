d28e5ef8773f7dc8c273b7db239198ee8e96f2ba
Alkasai <Alkasai@users.noreply.github.com>
2025-01-27 14:24:22 -0500

    Update UnifiedStorage.ts

--- vuejs/src/UnifiedStorage.ts
+++ vuejs/src/UnifiedStorage.ts
@@ -3,33 +3,45 @@ UnifiedStorage
 ==============
 File storage
 */
-export const FileType = ['FILE', 'FOLDER'] as const
+export const FileType = [
+	'FILE',
+	'FOLDER'
+] as const;
 
-export type TFileType = (typeof FileType)[number]
+export type TFileType = typeof FileType[number];
 
-export const PermissionRole = ['OWNER', 'READ', 'WRITE'] as const
+export const PermissionRole = [
+	'OWNER',
+	'READ',
+	'WRITE'
+] as const;
 
-export type TPermissionRole = (typeof PermissionRole)[number]
+export type TPermissionRole = typeof PermissionRole[number];
 
 export interface IStorageFile {
-  id?: string
-  created_at?: string | Date
-  updated_at?: string | Date
-  name: string
-  description?: string
-  parent_id?: string
-  user_id?: string
-  size?: number
-  type: TFileType
-  mime_type?: string
-  permissions?: IStoragePermission[]
-  download_url?: string
-  hash?: string
-  raw?: unknown
+	id?: string;
+	created_at?: string | Date | number;
+	updated_at?: string | Date | number;
+	name: string;
+	description?: string;
+	parent_id?: string;
+	user_id?: string;
+	size?: number;
+	type: TFileType;
+	mime_type?: string;
+	permissions?: IStoragePermission[];
+	download_url?: string; // This link expires after 1 hour. When you need to retrieve the contents of a file, call the "Retrieve a file" endpoint again to get a new download_url.
+	hash?: string;
+	data?: string; // base64 encoded file contents used to create/update only
+	version?: string;
+	web_url?: string;
+	raw?: unknown;
 }
 
 export interface IStoragePermission {
-  user_id?: string
-  group_id?: string
-  roles: TPermissionRole[]
+	user_id?: string;
+	group_id?: string;
+	roles: ( TPermissionRole )[];
+	is_hidden?: boolean;
+	is_public?: boolean;
 }
